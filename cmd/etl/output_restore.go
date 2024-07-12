faca40a0711fda6bf1fe6a2781e7faf3830bc279
Tit Petric <tit@tyk.io>
2024-07-13 00:07:59 +0200

    implement output restore

--- cmd/etl/output_restore.go
+++ cmd/etl/output_restore.go
@@ -2,9 +2,47 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path"
+
+	"github.com/jmoiron/sqlx"
+
+	. "github.com/titpetric/etl/internal/model"
+	"github.com/titpetric/etl/internal/repository"
 )
 
-// outputRestore writes files to `config.Folder`, stdin should have .commit_id in JSON.
+// outputRestore reads a Commit from stdin, fetches the outputs for that commit ID, and restores the folder
 func outputRestore(ctx context.Context, command *Command) error {
+	// Read from stdin
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+
+	var commit Commit
+	if err := json.Unmarshal(input, &commit); err != nil {
+		return err
+	}
+
+	db, err := sqlx.Open("sqlite3", config.DSN)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	repo := repository.NewOutputRepository(db)
+	outputs, err := repo.ListByCommitID(ctx, commit.ID)
+	if err != nil {
+		return err
+	}
+
+	for _, output := range outputs {
+		if err := os.WriteFile(path.Join(config.Folder, output.Filename), []byte(output.Contents), os.ModePerm); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
